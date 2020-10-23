e3eedc152b507bc606398029d3bba43084345a9e
Harunobu Ishii <harunobuishii547@gmail.com>
2020-10-23 10:07:38 +0800

    Update

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template, redirect, flash
+from flask_ngrok import run_with_ngrok
 
 from summarize import generate_summary
-from tax import CanadaTax
+from CAtax import CanadaTax
+from SGtax import SGtax
 
 #Create the flask object
 app = Flask(__name__)
+run_with_ngrok(app)
 
 #create route
 @app.route('/')
@@ -19,16 +22,21 @@ def summarize():
       message=request.form['message']
       output= generate_summary(message, top_n=int(selectedValue))
       # output= generate_summary(message, top_n=3)
-      return render_template('summarize.html', summarized_text=output, original_text=message, TextA="Here's your summary", TextB="Original text" )
+      return render_template('summarize.html', summarized_text=output, original_text=message, TextA="Your summary", TextB="Original text" )
    else:
       return render_template('summarize.html')
 
 @app.route('/taxcalc', methods=['POST', "GET"])
 def taxcalc():
+   selectedCountry = str(request.form.get('country'))
    if request.method=='POST':
       income=int(request.form['income'])
-      output= CanadaTax(income)
-      return render_template('tax.html',taxamount="Your Total tax is: ${:,.0f}".format(output), salary="Based on your income: ${:,.0f}".format(income))
+      output=0
+      if selectedCountry=="Canada":
+         output = CanadaTax(income)
+      elif selectedCountry=="Singapore":
+         output = SGtax(income)
+      return render_template('tax.html',taxamount="Your Total tax" +" in "+selectedCountry +" is: ${:,.0f}".format(output), salary="Based on your income: ${:,.0f}".format(income), country=selectedCountry)
    else:
       return render_template('tax.html')
 
